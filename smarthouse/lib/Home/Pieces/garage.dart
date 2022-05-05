deb3523b9816a6b4e4b04bf334ed789f4bebc795
uvsq21703728 <ahmed.djeblahi@ens.uvsq.fr>
2022-05-05 17:42:15 +0200

    maj 213

--- smarthouse/lib/Home/Pieces/garage.dart
+++ smarthouse/lib/Home/Pieces/garage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smarthouse/Home/Alarme/alarm.dart';
 import 'package:smarthouse/Home/Led/allLed.dart';
+import 'package:smarthouse/Home/Porte/allPorte.dart';
 import 'package:smarthouse/Home/Volets/allVolet.dart';
 
 class garage extends StatefulWidget {
@@ -25,8 +27,9 @@ class _garageState extends State<garage> {
                      crossAxisAlignment: CrossAxisAlignment.center,
                      children: [Text("LEDS"),
                        ledGarage(),
-                       Text("VOLETS"),voletGarage()
-
+                       Text("VOLETS"),voletGarage(),
+                        Text("Alarme"),alarmeGarage(),
+                         Text("Porte"),porteGarage()
                      ]
                      )
                      )
